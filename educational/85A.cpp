82eca856bc26ecd3adb5e92635ef42bda2a905f6
petite_prog <tkkydrm3tn@yahoo.co.jp>
2020-04-11 04:16:00 +0900

    Educational #85

---
+++ educational/85A.cpp
@@ -0,0 +1,111 @@
+/*
+　　　∫ ∫ ∫
+　　　ノヽ
+　　（＿　 ）
+　（＿　　　 ）
+（＿＿＿＿＿＿ ）
+　ヽ(´･ω･)ﾉ　
+　　 |　 /
+　　 UU
+*/
+#pragma region macro
+#include <bits/stdc++.h>
+typedef long long int64;
+using namespace std;
+using P = pair<int64, int64>;
+typedef vector<int> vi;
+const int MOD = (int)1e9 + 7;
+const int64 INF = 1LL << 62;
+const int inf = 1<<30;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define FOR(i,s,n) for (int i = s; i < (n); i++)
+#define ALL(obj) (obj).begin(), (obj).end() //コンテナじゃないと使えない!!
+#define debug(x) cerr << #x << ": " << x << "\n";
+#define mp make_pair
+#define bn '\n'
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T> &V){
+    int N = V.size();
+    REP(i,N){
+        os << V[i];
+        if (i!=N-1) os << " ";
+    }
+    os << "\n";
+    return os;
+}
+template <typename T,typename S>
+ostream& operator<<(ostream& os, pair<T,S> const&P){
+    os << "(";
+    os << P.first;
+    os << " , ";
+    os << P.second;
+    os << ")";
+    return os;
+}
+template <typename T>
+ostream& operator<<(ostream& os, set<T> &S){
+    auto it=S.begin();
+    while(it!=S.end()){
+        os << *it;
+        os << " ";
+        it++;
+    }
+    os << "\n";
+    return os;
+}
+template <typename T>
+ostream& operator<<(ostream& os, deque<T> &q){
+    for(auto it=q.begin();it<q.end();it++){
+        os<<*it;
+        os<<" ";
+    }
+    os<<endl;
+    return os;
+}
+template <typename T,typename S>
+ostream& operator<<(ostream& os, map<T,S> const&M){
+    for(auto e:M){
+        os<<e;
+    }
+    os<<endl;
+    return os;
+}
+vector<pair<int,int>> dxdy = {mp(0,1),mp(1,0),mp(-1,0),mp(0,-1)};
+#pragma endregion
+//fixed<<setprecision(10)<<ans<<endl;
+
+void solve(){
+    int N; cin >> N;
+    int before_p = 0, before_c = 0;
+    int p,c;
+    vector<pair<int,int>> PC;
+    REP(i,N) cin >> p >> c, PC.emplace_back(p,c);
+    REP(i,N){
+        tie(p,c) = PC[i];
+        if(p<c){
+            cout << "NO" << bn;
+            return;
+        }else{
+            if(before_p > p or before_c > c){
+                cout << "NO" << bn;
+                return;
+            }
+            if(p-before_p < c-before_c){
+                cout << "NO" << bn;
+                return; 
+            }
+        }
+        before_p = p, before_c = c;
+    }
+    cout << "YES" << bn;
+}
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int N;
+    cin >> N;
+    REP(i,N) solve();
+}
---
+++ educational/85D.cpp
@@ -0,0 +1,116 @@
+/*
+　　　∫ ∫ ∫
+　　　ノヽ
+　　（＿　 ）
+　（＿　　　 ）
+（＿＿＿＿＿＿ ）
+　ヽ(´･ω･)ﾉ　
+　　 |　 /
+　　 UU
+*/
+#pragma region macro
+#include <bits/stdc++.h>
+typedef long long int64;
+using namespace std;
+using P = pair<int64, int64>;
+typedef vector<int> vi;
+const int MOD = (int)1e9 + 7;
+const int64 INF = 1LL << 62;
+const int inf = 1<<30;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define FOR(i,s,n) for (int i = s; i < (n); i++)
+#define ALL(obj) (obj).begin(), (obj).end() //コンテナじゃないと使えない!!
+#define debug(x) cerr << #x << ": " << x << "\n";
+#define mp make_pair
+#define bn '\n'
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T> &V){
+    int N = V.size();
+    REP(i,N){
+        os << V[i] + 1;
+        if (i!=N-1) os << " ";
+    }
+    os << "\n";
+    return os;
+}
+template <typename T,typename S>
+ostream& operator<<(ostream& os, pair<T,S> const&P){
+    os << "(";
+    os << P.first;
+    os << " , ";
+    os << P.second;
+    os << ")";
+    return os;
+}
+template <typename T>
+ostream& operator<<(ostream& os, set<T> &S){
+    auto it=S.begin();
+    while(it!=S.end()){
+        os << *it;
+        os << " ";
+        it++;
+    }
+    os << "\n";
+    return os;
+}
+template <typename T>
+ostream& operator<<(ostream& os, deque<T> &q){
+    for(auto it=q.begin();it<q.end();it++){
+        os<<*it;
+        os<<" ";
+    }
+    os<<endl;
+    return os;
+}
+template <typename T,typename S>
+ostream& operator<<(ostream& os, map<T,S> const&M){
+    for(auto e:M){
+        os<<e;
+    }
+    os<<endl;
+    return os;
+}
+vector<pair<int,int>> dxdy = {mp(0,1),mp(1,0),mp(-1,0),mp(0,-1)};
+#pragma endregion
+//fixed<<setprecision(10)<<ans<<endl;
+vector<int> ans;
+int64 N,L,R;
+void dfs(int64& now,int cur){
+    if(cur==N-1 or now > R) return;
+    if(L-now >= 2*(N-cur-1)){
+        now+=2*(N-cur-1);
+    }else{
+        REP(i,2*(N-cur-1)){
+            if(now<L or R<now) {now++;continue;}
+            if(i&1){
+                ans.emplace_back(i/2 + cur + 1);
+            }else{
+                ans.emplace_back(cur);
+            }
+            now++;
+        }
+    }
+
+    dfs(now,cur+1);
+}
+
+void solve(){
+    cin >> N >> L >> R;
+    ans.clear();
+    int64 now = 1;
+    dfs(now,0);
+    if(L <= now and now <= R){
+        ans.emplace_back(0);
+    }
+    cout << ans;
+}
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int N;
+    cin >> N;
+    REP(i,N) solve();
+}
